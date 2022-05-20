3076be5bd0f53e14916a5ad51ffc5763fb7ce279
FreddieHorn <67193178+FreddieHorn@users.noreply.github.com>
2022-05-20 11:55:47 +0200

    Initial login screen

--- simulator_v1/Form1.Designer.cs
+++ simulator_v1/Form1.Designer.cs
@@ -28,13 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.login_button = new System.Windows.Forms.Button();
+            this.password_box = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.username_label = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.version = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(304, 373);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(167, 56);
+            this.login_button.TabIndex = 0;
+            this.login_button.Text = "Sign in";
+            this.login_button.UseVisualStyleBackColor = true;
+            // 
+            // password_box
+            // 
+            this.password_box.Location = new System.Drawing.Point(284, 347);
+            this.password_box.Name = "password_box";
+            this.password_box.Size = new System.Drawing.Size(213, 20);
+            this.password_box.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(284, 312);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 315);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username:";
+            // 
+            // username_label
+            // 
+            this.username_label.AutoSize = true;
+            this.username_label.Location = new System.Drawing.Point(220, 350);
+            this.username_label.Name = "username_label";
+            this.username_label.Size = new System.Drawing.Size(56, 13);
+            this.username_label.TabIndex = 4;
+            this.username_label.Text = "Password:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::simulator_v1.Properties.Resources.login;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(145, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(498, 233);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // version
+            // 
+            this.version.AutoSize = true;
+            this.version.Location = new System.Drawing.Point(12, 9);
+            this.version.Name = "version";
+            this.version.Size = new System.Drawing.Size(58, 13);
+            this.version.TabIndex = 6;
+            this.version.Text = "Alpha v0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(100, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(591, 39);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Space Shuttle Pilot Control System";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.version);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.username_label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.password_box);
+            this.Controls.Add(this.login_button);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.TextBox password_box;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label username_label;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label version;
+        private System.Windows.Forms.Label label2;
     }
 }
 
